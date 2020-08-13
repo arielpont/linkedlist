fc6df971af2cb74856539708c0557cac8a7616cb
Ariel Pont <arielpont@hotmail.com>
2020-08-13 20:53:13 -0300

    First commit

---
+++ app.py
@@ -0,0 +1,53 @@
+import os
+
+class LinkedList:
+    def __init__(self, nodes=None):
+        self.head = None
+        if nodes is not None:
+            node = Node(data=nodes.pop(0))
+            self.head = node
+            for elem in nodes:
+                node.next = Node(data=elem)
+                node = node.next
+    
+    def __repr__(self):
+        node = self.head
+        nodes = []
+        while node is not None:
+            nodes.append(node.data)
+            node = node.next
+        nodes.append("None")
+        return " -> ".join(nodes)
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def __repr__(self):
+        return self.data
+
+def clear():
+    """ Clear the console on Windows, MacOS and Linux. """
+
+    # windows 
+    if os.name == "nt": 
+        _ = os.system("cls") 
+    # mac and linux
+    else: 
+        _ = os.system("clear")
+
+clear()
+
+llist = LinkedList()
+print(llist)
+
+first_node = Node("a")
+llist.head = first_node
+print(llist)
+
+second_node = Node("b")
+third_node = Node("c")
+first_node.next = second_node
+second_node.next = third_node
+print(llist)
